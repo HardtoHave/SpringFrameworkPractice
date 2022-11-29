7ab26edc01b95993b4fdaa2e7dadc2e9129ba6a2
robert <robert.ljx.de.simon@gmail.com>
2022-11-29 17:32:34 +1100

    scope test

---
+++ src/test/java/org/example/spring/test/ScopeTest.java
@@ -0,0 +1,16 @@
+package org.example.spring.test;
+
+import org.example.spring.pojo.Student;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class ScopeTest {
+    @Test
+    public void testScope(){
+        ApplicationContext ioc=new ClassPathXmlApplicationContext("spring-scope.xml");
+        Student bean = ioc.getBean(Student.class);
+        Student student=ioc.getBean(Student.class);
+        System.out.println(bean==student);
+    }
+}
